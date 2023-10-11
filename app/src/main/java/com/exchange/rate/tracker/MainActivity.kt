e85dc4c22148b6b022693796cc4a65575dae57fb
= <lara@maditinternational.com>
2023-10-11 21:47:33 +0400

    Create project

---
+++ app/src/main/java/com/exchange/rate/tracker/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.exchange.rate.tracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.exchange.rate.tracker.ui.theme.ExchangeRateTrackerTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      ExchangeRateTrackerTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+
+        }
+      }
+    }
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+  ExchangeRateTrackerTheme {
+
+  }
+}
